8c723cb0829a2508c114a9fa038b3feab2f11abd
Brian Murphy-Dye <brian@murphydye.com>
2017-01-13 15:37:19 -0700

    Delete unwanted columns from spreadsheet.

--- src/clj/summit/sap/spreadsheet.clj
+++ src/clj/summit/sap/spreadsheet.clj
@@ -14,14 +14,41 @@
           ))
 ;; (temp-filename "abc" "xlsx")
 
+(defn delete-col-nums
+  ([delete-cols] (fn [row] (delete-col-nums delete-cols row)))
+  ([delete-cols row]
+   delete-cols
+   (->>
+    (map #(if (contains? delete-cols %2) :delete-it %1) row (range))
+    (remove #(= % :delete-it))
+    )))
+;; (delete-col-nums #{2 9} (range 15))
+;; (->> (remove-deleted-columns all-data) (take 3))
+
+(defn remove-deleted-columns
+  [data]
+  (let [deleted-col-nums (->>
+                          (map (fn [col colnum] (if (= col "--DELETE--") colnum))
+                               (first data)
+                               (range))
+                          (remove nil?)
+                          set)
+        ]
+    (if (empty? deleted-col-nums)
+      data
+      (map (delete-col-nums deleted-col-nums) data)))
+  )
+
 (defn create-temp-spreadsheet
   "returns the filename of the created spreadsheet"
   [prefix wb-name colnames data]
   (let [filename (temp-filename prefix "xlsx")
         data (concat [(map u/->str colnames)] data)
-        _ (println data)
+        data (remove-deleted-columns data)
+        ;; _ (println data)
         wb (xls/create-workbook wb-name
                                 data)]
+  (def all-data data)
     (xls/save-workbook! filename wb)
     filename))
 ;; (create-temp-spreadsheet "status-lines" "Status Lines"
